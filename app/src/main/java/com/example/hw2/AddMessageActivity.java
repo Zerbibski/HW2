18d79a7ca334400e7fb5cb1fc6ba3e4a80fd7729
Zerbibski <ethan.zerbib@post.runi.ac.il>
2024-06-24 20:55:34 +0300

    Homework

--- app/src/main/java/com/example/hw2/AddMessageActivity.java
+++ app/src/main/java/com/example/hw2/AddMessageActivity.java
@@ -30,78 +30,24 @@ public class AddMessageActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     ImageView addPhoto;
     int REQUEST_PERMISSIONS_CODE = 1;
-    private ActivityResultLauncher<Uri> takePictureLauncher;
-    private Uri CurrentImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_message);
 
-        takePictureLauncher = registerForActivityResult(new ActivityResultContracts.TakePicture(), result -> {
-            if (result) {
-                addPhoto.setImageURI(CurrentImage);
-            }
-        });
-        addPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 
-                    ActivityCompat.requestPermissions(AddMessageActivity.this, new String[] {
-                            Manifest.permission.CAMERA,
-                            Manifest.permission.ACCESS_MEDIA_LOCATION
-                    }, REQUEST_PERMISSIONS_CODE);
-                } else {
-                    captureImage();
-                }
-            }
-        });
         Button btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText name = findViewById(R.id.name);
                 EditText text = findViewById(R.id.text);
-                StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-                StorageReference fileRef = storageRef.child(CurrentImage.getPath());
-                fileRef.putFile(CurrentImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                Message c = new Message(uri.toString(),name.getText().toString(),text.getText().toString());
-                                db.collection("Messages").document(c.ID).set(c.getAsMap());
-                                Intent i = new Intent();
-                                i.putExtra("message",c);
-                                setResult(1,i);
-                                finish();
-                            }
-                        });
-                    }
-                });
+                Message c = new Message("Value",name.getText().toString(),text.getText().toString());
+                db.collection("Messages").document(c.ID).set(c.getAsMap());
+                Intent i = new Intent();
+                i.putExtra("message",c);
+                setResult(1,i);
+                finish();}});}
 
-
-
-
-
-            }
-        });
-    }
-
-    private Uri createImageUri() {
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, "New Picture");
-        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
-        return getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-    }
-
-    public void captureImage() {
-        Uri imageUri = createImageUri();
-        if (imageUri != null) {
-            CurrentImage = imageUri;
-            takePictureLauncher.launch(imageUri);
-        }
-    }
 }
